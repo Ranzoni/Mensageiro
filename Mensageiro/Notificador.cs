1358e7e093a19d648bcff63dc395a718a06a5557
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2024-08-27 09:04:13 -0300

    Método para adicionar mensagens de Não Autorizado ao notificador

--- Mensageiro/Notificador.cs
+++ Mensageiro/Notificador.cs
@@ -5,9 +5,21 @@ namespace Mensageiro
     /// <summary>
     /// Classe responsável por gerenciar mensagens de notificação.
     /// </summary>
-    public class Notificador : NotificadorBase, INotificador
+    public class Notificador : INotificador
     {
+        const string MSG_PADRAO = "PADRAO";
         const string MSG_NAO_ENCONTRADO = "NAO_ENCONTRADO";
+        const string MSG_NAO_AUTORIZADO = "MSG_NAO_AUTORIZADO";
+
+        private readonly Dictionary<string, string> _mensagens = [];
+
+        private void AddMensagemLista(string mensagem, string chave = MSG_PADRAO)
+        {
+            if (string.IsNullOrEmpty(chave) || string.IsNullOrWhiteSpace(chave))
+                chave = MSG_PADRAO;
+
+            _mensagens.Add(chave, mensagem);
+        }
 
         /// <summary>
         /// Adiciona uma mensagem do tipo string ao notificador.
@@ -80,7 +92,7 @@ namespace Mensageiro
         /// <returns></returns>
         public bool ExisteMensagem()
         {
-            return ListaMensagens().Count > 0;
+            return _mensagens.Count > 0;
         }
 
         /// <summary>
@@ -89,8 +101,7 @@ namespace Mensageiro
         /// <returns></returns>
         public bool ExisteMsgNaoEncontrado()
         {
-            return ListaMensagens()
-                .Any(m => m.Key.Equals(MSG_NAO_ENCONTRADO));
+            return _mensagens.Any(m => m.Key.Equals(MSG_NAO_ENCONTRADO));
         }
 
         /// <summary>
@@ -99,7 +110,7 @@ namespace Mensageiro
         /// <returns></returns>
         public IEnumerable<string> Mensagens()
         {
-            return ListaMensagens().Values;
+            return _mensagens.Values;
         }
 
         /// <summary>
@@ -108,8 +119,63 @@ namespace Mensageiro
         /// <returns></returns>
         public IEnumerable<string> MensagensDeNaoEncontrado()
         {
-            return ListaMensagens()
+            return _mensagens
                 .Where(m => m.Key.Equals(MSG_NAO_ENCONTRADO))
+                .Select(m => m.Value);
+        }
+
+        /// <summary>
+        /// Adiciona uma mensagem de não autorizado do tipo string ao notificador.
+        /// É o mesmo efeito do AddMensagem, porém é adicionada uma identificação para a mensagem de "não autorizado".
+        /// </summary>
+        /// <param name="mensagem"></param>
+        public void AddMensagemNaoAutorizado(string mensagem)
+        {
+            if (string.IsNullOrEmpty(mensagem) || string.IsNullOrWhiteSpace(mensagem))
+                return;
+
+            AddMensagemLista(
+                mensagem: mensagem,
+                chave: MSG_NAO_AUTORIZADO);
+        }
+
+        /// <summary>
+        /// Adiciona uma mensagem de não autorizado do tipo enum ao notificador.
+        /// É o mesmo efeito do AddMensagem, porém é adicionada uma identificação para a mensagem de "não autorizado".
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mensagem"></param>
+        public void AddMensagemNaoAutorizado<T>(T mensagem) where T : struct, IConvertible
+        {
+            var mensagemEmString = mensagem.Descricao();
+            if (string.IsNullOrEmpty(mensagemEmString) || string.IsNullOrWhiteSpace(mensagemEmString))
+            {
+                MensageiroExcecao.MensagemNotificacaoVazia();
+                return;
+            }
+
+            AddMensagemLista(
+                mensagem: mensagemEmString,
+                chave: MSG_NAO_AUTORIZADO);
+        }
+
+        /// <summary>
+        /// Retorna verdadeiro caso exista alguma mensagem no notificador do tipo 'não autorizado', caso contrário retorna falso.
+        /// </summary>
+        /// <returns></returns>
+        public bool ExisteMsgNaoAutorizado()
+        {
+            return _mensagens.Any(m => m.Key.Equals(MSG_NAO_AUTORIZADO));
+        }
+
+        /// <summary>
+        /// Retorna todas as mensagens de não autorizado do notificador.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> MensagensDeNaoAutorizado()
+        {
+            return _mensagens
+                .Where(m => m.Key.Equals(MSG_NAO_AUTORIZADO))
                 .Select(m => m.Value);
         }
     }
