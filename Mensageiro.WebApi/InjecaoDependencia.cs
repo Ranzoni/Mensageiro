014ef026253223cb4ca768ac58f80cad17603bb4
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2025-01-02 08:31:34 -0300

    Método facilitador para fazer a injeção de dependência do notificador

---
+++ Mensageiro.WebApi/InjecaoDependencia.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Mensageiro.WebApi
+{
+    /// <summary>
+    /// Classe estática responsável por fornecer ações para a injeção de dependência da API .NET
+    /// </summary>
+    public static class InjecaoDependencia
+    {
+        /// <summary>
+        /// Método responsável por fazer a injeção de dependência do Notificador
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddMensageiro(this IServiceCollection services)
+        {
+            services.AddTransient<INotificador, Notificador>();
+        }
+    }
+}
